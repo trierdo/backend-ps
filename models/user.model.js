ea1906805b1fc1096235c9dfd35efe131fa07fc7
Stefan Grulke <grulke@mediamarktsaturn.com>
2019-10-17 14:39:14 +0200

    creating models

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create an object to create, read, update and delete asset data in the MongoDB
+
+let User = new Schema({
+    //we do not need to define the _id to identify the asset, mongoose does this automatically
+    user_first_name: {
+        type: String
+    },
+    user_last_name: {
+        type: String
+    },
+    user_password: {
+        type: String
+    },
+    user_email: {
+        type: String
+    },
+    user_phone: {
+        type: String
+    },
+    ref_fav_products:
+        [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  
+});
+
+module.exports = mongoose.model('User', User);
