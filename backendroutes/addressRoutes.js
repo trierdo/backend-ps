45a90a90b6710e73b90201f9e32e16cf106a7ce7
Stefan Grulke <grulke@mediamarktsaturn.com>
2019-10-29 15:51:12 +0100

    address backendroutes updated

--- backendroutes/addressRoutes.js
+++ backendroutes/addressRoutes.js
@@ -2,26 +2,43 @@ const express = require('express');
 const addressRoutes = express.Router();
 const Address = require("../models/address.model");
 const ObjectId = require('mongodb').ObjectID;
+const mongoose = require('mongoose');
 
 
-addressRoutes.route('/getAddressData/:userID').get((req, res) => {
-    Address.findOne({"ref_user" : ObjectId(req.params.userID)})
+addressRoutes.route('/getAddressData/:userId').get((req, res) => {
+  console.log("req.params.userId",req.params.userId)
+    Address.find({"ref_user" : ObjectId(req.params.userId)})
     .then(addressDataFromDB => {
         res.status(200).json({'addressData': addressDataFromDB});
     })
     .catch(error => {
 
-     console.log(error) ;
+     console.log("Keine Adresse?",error) ;
     })
   });
 
-  addressRoutes.route("/createAdress/:userID").post((req, res) => {
+  addressRoutes.route('/deleteAddressData/:addressId').get((req, res) => {
+    console.log("req.params.addressId",req.params.addressId)
+      Address.findOneAndDelete({"_id" : ObjectId(req.params.addressId)})
+      .then(addressDataFromDB => {
+          res.status(200).json({'addressData': addressDataFromDB});
+      })
+      .catch(error => {
+  
+       console.log("Keine Adresse?",error) ;
+      })
+    });
+
+  addressRoutes.route("/createAddress").post((req, res) => {
+    console.log("req.body für create address",req.body)
+    
+    
     const type = req.body.type;
     const street = req.body.street;
     const zip_code = req.body.zip_code;
     const city = req.body.city;
     const iso_country_code = req.body.iso_country_code;
-    const ref_user = ObjectId(req.params.userID);
+    const ref_user = req.body.ref_user;
     const pickup_station_id = req.body.pickup_station_id;
 
     const pickup_ident_no = req.body.pickup_ident_no;
@@ -32,6 +49,7 @@ addressRoutes.route('/getAddressData/:userID').get((req, res) => {
     if (empty_fields.length > 0 ) {
         return res.status(401).json({ 'error': 'all fields must be filled' });
     }
+    
     Address.create({
         _id: new mongoose.mongo.ObjectId(),
         type,
@@ -43,7 +61,8 @@ addressRoutes.route('/getAddressData/:userID').get((req, res) => {
         pickup_station_id,
         pickup_ident_no
         })
-        .then(() => { return res.status(200).json({ 'success': 'registration was successfull' });
+        .then(() => { return res.status(200).json({ 'success': 'registration was successfull'});
+
           })
       .catch(error => {
         console.log(error);
@@ -51,7 +70,7 @@ addressRoutes.route('/getAddressData/:userID').get((req, res) => {
 });
 
   // Bisher gibt es nur eine Addresse für einen User
-  addressRoutes.route('/editAdress/:userID').post((req, res) => {
+  addressRoutes.route('/editAddress/:userID').post((req, res) => {
     const { type, street, zip_code, city, iso_country_code, pickup_station_id, pickup_ident_no } = req.body;
     console.log("log: " + req.params.userID);
     Address.update({ref_user: ObjectId(req.params.userID)}, { $set: {type, street, zip_code, city, iso_country_code, pickup_station_id, pickup_ident_no }})
